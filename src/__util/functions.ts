f7e9e26e253bf28b754b4e1ab1687965e90d0280
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-06-11 16:51:25 +0200

    remove comments

--- src/__util/functions.ts
+++ src/__util/functions.ts
@@ -1,4 +1,3 @@
-// Returns a formatted string from the default text formatter, then removes additional perk-specific words or symbols
 export function formatPerkName(perkName: string): string {
   const formattedPerkName = defaultTextFormatter(perkName).replaceAll(
     'hex',
@@ -7,14 +6,11 @@ export function formatPerkName(perkName: string): string {
   return formattedPerkName;
 }
 
-// Returns a formatted string based on the default text formatter
 export function formatOwnerName(name: string): string {
   const formattedOwnerName = defaultTextFormatter(name);
   return formattedOwnerName;
 }
 
-// Default text formatter for things such as perk names, killer names, etc.
-// Used to improve leniency in user input when fetching data by comparing two heavily formatted strings
 export function defaultTextFormatter(unformattedInput: string): string {
   if (!unformattedInput) {
     return unformattedInput;
--- src/perks/perks.service.ts
+++ src/perks/perks.service.ts
@@ -9,7 +9,6 @@ export class PerksService {
   private readonly survivorPerks = [...perkData.survivor];
   private readonly killerPerks = [...perkData.killer];
 
-  // Returns all perks based on role (killer/survivor/all)
   async getAllPerks(role: Role): Promise<Perk[]> {
     if (!role) {
       return this.allPerks;
@@ -18,25 +17,20 @@ export class PerksService {
   }
 
   async getPerkData(name: string): Promise<Perk[]> {
-    // Format perk name to improve leniency when parsing all perks
     const formattedPerkName: string = formatPerkName(name);
 
-    // Attempt to find perk data based on the formatted perk names
     const perk: Perk | undefined = this.allPerks.find(
       (perk) => formatPerkName(perk.name) === formattedPerkName,
     );
 
-    // If no perk was found, throw a 400 error...
     if (!perk) {
       throw new BadRequestException(`Couldn't find the given perk: '${name}'`);
     }
 
-    // ...otherwise, return the perk data
     return [perk];
   }
 
   async getRandomPerks(role: Role, amount: number = 4): Promise<Perk[]> {
-    // Handle random perks if no role was specified
     if (!role) {
       const randomPerks = [];
       for (let i = 0; i < amount; i++) {
@@ -48,39 +42,30 @@ export class PerksService {
       return randomPerks;
     }
 
-    // Define valid roles
     // (TODO: Move this to a better place)
     const validRoles = ['killer', 'survivor'];
 
-    // Handle invalid roles
     if (!validRoles.includes(role)) {
       throw new BadRequestException();
     }
 
-    // Create boolean for is the targeted role is killer or not
-    // This is used to check which perks to fetch later on
     const isKiller = role === 'killer';
 
-    // Define perks based on the isKiller variable
     const allRolePerks = isKiller
       ? [...this.killerPerks]
       : [...this.survivorPerks];
 
-    // Loop through the "amount" variable, and fetch a random perk that many times
     const randomPerks: Perk[] = [];
     for (let i = 0; i < amount; i++) {
-      // Ensure there are still remaining perks to fetch from
       if (allRolePerks.length <= 0) {
         break;
       }
 
-      // Get a random entry from all perks for the specified role, then remove that entry from allRolePerks
       const randomIndex = Math.floor(Math.random() * allRolePerks.length);
       randomPerks.push(allRolePerks[randomIndex]);
       allRolePerks.splice(randomIndex, 1);
     }
 
-    // Finally, return the randomPerks array
     return randomPerks;
   }
 }
